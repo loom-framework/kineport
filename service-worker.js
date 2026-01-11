6b1502d5eb1a033aba7572b537dfbb48cb212c14
loom-framework <loom.framework@gmail.com>
2026-01-11 20:21:03 +0100

    hghg
    jhj

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
+// -----------------------------------------------------------------------------
+// Description: Application bootstrap and initialization logic.
+// Author: Janis Bedeicis
+// Github: https://github.com/loom-framework
+// E-mail: loom.framework@gmail.com
+// Created: 2008
+// -----------------------------------------------------------------------------
+
 // --- Versioning -------------------------------------------------------------
 const SW_VERSION = "v36"; 
 const CACHE_NAME = `kineport-app-cache-${SW_VERSION}`;
