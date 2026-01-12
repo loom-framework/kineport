f4732f27cd8d7929c3399a7aac214358dbd97aac
loom-framework <loom.framework@gmail.com>
2026-01-12 06:41:00 +0100

    sf
    fs

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 // -----------------------------------------------------------------------------
 
 // --- Versioning -------------------------------------------------------------
-const SW_VERSION = "v45"; 
+const SW_VERSION = "v46"; 
 const CACHE_NAME = `kineport-app-cache-${SW_VERSION}`;
 
 const PRECACHE_URLS = [
