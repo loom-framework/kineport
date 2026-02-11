ed5b7ba2fb128471422c12b8e8da6d9dc478e288
loom-framework <loom.framework@gmail.com>
2026-02-11 19:30:30 +0200

    fsfds
    dsfds

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 // -----------------------------------------------------------------------------
 
 // --- Versioning -------------------------------------------------------------
-const SW_VERSION = "v62"; 
+const SW_VERSION = "v63"; 
 const CACHE_NAME = `kineport-app-cache-${SW_VERSION}`;
 
 const PRECACHE_URLS = [
