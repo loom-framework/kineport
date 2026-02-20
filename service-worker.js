3fbaecde8e25cf98c7cae1e627309cf4dec230c5
loom-framework <loom.framework@gmail.com>
2026-02-20 05:16:39 +0200

    sadsa
    sadas

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 // -----------------------------------------------------------------------------
 
 // --- Versioning -------------------------------------------------------------
-const SW_VERSION = "v70"; 
+const SW_VERSION = "v71"; 
 const CACHE_NAME = `kineport-app-cache-${SW_VERSION}`;
 
 const PRECACHE_URLS = [
