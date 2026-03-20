d2c629fa28e238b3e778f3e2aab73584231d45f9
loom-framework <loom.framework@gmail.com>
2026-03-20 12:29:41 +0200

    rewr
    erwew

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 // -----------------------------------------------------------------------------
 
 // --- Versioning -------------------------------------------------------------
-const SW_VERSION = "v91"; 
+const SW_VERSION = "v92"; 
 const CACHE_NAME = `kineport-app-cache-${SW_VERSION}`;
 
 const PRECACHE_URLS = [
