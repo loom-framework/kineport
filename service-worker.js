397aebb6440646c6a8dcdd1a68bb59e1b3758ee5
loom-framework <144352200+loom-framework@users.noreply.github.com>
2026-08-13 08:32:58 +0000

    eeewww

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 // -----------------------------------------------------------------------------
 
 // --- Versioning -------------------------------------------------------------
-const SW_VERSION = "v100"; 
+const SW_VERSION = "v101"; 
 const CACHE_NAME = `kineport-app-cache-${SW_VERSION}`;
 
 const PRECACHE_URLS = [
